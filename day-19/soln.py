1b1e8644439ba624fc08742f58c893f057aa0453
Julia Evans <julia@jvns.ca>
2020-12-19 12:35:56 -0500

    part 2

--- day-19/soln.py
+++ day-19/soln.py
@@ -16,19 +16,32 @@ def parse(line):
 
 import re
 
-def part1(input):
+def part2(input):
     rules, strings = input.split('\n\n')
     rules = dict(parse(x) for x in rules.split('\n'))
-    reg = '^' + regex(0, rules) + '$'
-    print(reg)
+    r42 = regex(42, rules)
+    r31 = regex(31, rules)
     # 0: 8 11
+    # this is (r42)+
     # 8: 42 | 42 8
+    # this is equal numbers of things that match 42 / 31
     # 11: 42 31 | 42 11 31
-    reg = re.compile(reg)
-    total = sum(1 for x in strings.strip().split('\n') if reg.match(x))
+    total = sum(1 for x in strings.strip().split('\n') if match(x, r42, r31))
     print(total)
 
+def match(x, r42, r31):
+    for i in range(1, 30):
+        # equal numbers of things that match 42 / 31, so this is a hack to try
+        # every possible number. we probably don't need to go as big as 30
+        # because the strings aren't that long
+        regexp = f"^({r42})+(({r42}){{{i}}}({r31}){{{i}}})$"
+        if re.compile(regexp).match(x):
+            return True
+    return False
+
+
 def regex(num, rules):
+    # wrong guesses: 174, 274
     rule = rules[num]
     if type(rule) is str:
         return rule
@@ -41,4 +54,4 @@ def regex(num, rules):
         return f"({left}|{right})"
 
 
-part1(sys.stdin.read().strip())
+part2(sys.stdin.read().strip())

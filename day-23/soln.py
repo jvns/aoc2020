aa8c80e1cfae5c7d0a6da376e8f75a5838853842
Julia Evans <julia@jvns.ca>
2020-12-23 22:42:34 -0500

    uh a linked list???

--- day-23/soln.py
+++ day-23/soln.py
@@ -4,38 +4,65 @@ from typing import List
 from collections import deque
 
 @dataclass
-class Circle:
-    nums: object
-    max_val: int
+class LL:
+    value: int
+    next: object
 
-    def remove(self):
-        num = self.nums[1]
-        del self.nums[1]
-        return num
+@dataclass
+class Circle:
+    current: object
+    items: object
+    max_value: int
+
+    @staticmethod
+    def create(nums):
+        lls = [LL(n, None) for n in nums]
+        d = {}
+        for i, n in enumerate(lls):
+            d[n.value] = n
+            if i == len(nums) - 1:
+                lls[i].next = lls[0]
+            else:
+                lls[i].next = lls[i+1]
+        return Circle(lls[0], d, max(nums))
 
     def step(self):
-        cups = [self.remove() for i in range(3)]
-        dest = self.nums[0]
-        dest = (dest - 2) % (self.max_val) + 1
-        while dest not in self.nums:
-            dest = (dest - 2) % self.max_val + 1
-        idx = self.nums.index(dest)
-        self.nums = deque(list(self.nums)[:idx+1] + cups + list(self.nums)[idx+1:])
-        self.nums.append(self.nums.popleft())
+        n = self.current.next
+        nexts = [n.value, n.next.value, n.next.next.value]
+        dest = (self.current.value - 2) % self.max_value + 1
+        while dest in nexts:
+            dest = (dest - 2) % self.max_value + 1
+        target = self.items[dest]
 
-    def result(self):
-        d = self.nums.copy()
-        while d[0] != 1:
-            d.append(d.popleft())
-        return ''.join([str(x) for x in list(d)[1:]])
+        self.current.next = self.current.next.next.next.next
+        n.next.next.next = target.next
+        target.next = n
+        self.current = self.current.next
 
     def __str__(self):
-        return str(self.nums)
+        start = self.current
+        items = []
+        items.append(str(start.value))
+        x = start.next
+        while x is not start:
+            items.append(str(x.value))
+            x = x.next
+        return ' '.join(items)
 
+
+    def result(self):
+        start = self.items[1]
+        items = []
+        x = start.next
+        while x != start:
+            items.append(str(x.value))
+            x = x.next
+        return ''.join(items)
 
 def part1(input):
-    nums = deque([int(x) for x in input])
-    circle = Circle(nums, max(nums))
+    nums = [int(x) for x in input]
+    nums = nums + list(range(max(nums)+1, 1000 * 1000  +1))
+    circle = Circle.create(nums)
     for i in range(100):
         circle.step()
     print(circle.result())
